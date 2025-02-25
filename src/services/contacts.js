80cd19e8d2b43288a2f89786f7cbd649d58c3c92
Shylin Denis <tawnship406@gmail.com>
2025-02-25 15:31:03 +0200

    Добавил спмсок контактов в GBmongodb

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,9 +3,10 @@ const Contact = require('../models/Contact');
 const getAllContacts = async () => {
   try {
     const contacts = await Contact.find();
+    console.log('Найдено контактов:', contacts.length);
     return contacts;
   } catch (error) {
-    console.error('Service error - getAllContacts:', error);
+    console.error('Ошибка сервиса - getAllContacts:', error);
     throw error;
   }
 };
