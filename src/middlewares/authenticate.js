be02db9161aa5c857d1861174f148b49a51f28e9
Shylin Denis <tawnship406@gmail.com>
2025-03-29 16:08:46 +0200

    middlewares/authenticate.js controllers/auth.js - збільшив життя токена

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,14 +1,19 @@
-// authenticate.js;
 import jwt from 'jsonwebtoken';
 import createError from 'http-errors';
 import User from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
   try {
+    const isLogoutRequest = req.path === '/logout';
+
     const { authorization = '' } = req.headers;
     const [bearer, token] = authorization.split(' ');
 
     if (bearer !== 'Bearer' || !token) {
+      if (isLogoutRequest) {
+        req.user = { _id: null };
+        return next();
+      }
       return next(createError(401, 'Not authorized'));
     }
 
@@ -17,18 +22,31 @@ export const authenticate = async (req, res, next) => {
       const user = await User.findById(id);
 
       if (!user) {
+        if (isLogoutRequest) {
+          req.user = { _id: null };
+          return next();
+        }
         return next(createError(401, 'Not authorized'));
       }
 
       req.user = user;
       next();
     } catch (error) {
+      if (isLogoutRequest) {
+        req.user = { _id: null };
+        return next();
+      }
+
       if (error.name === 'TokenExpiredError') {
         return next(createError(401, 'Access token expired'));
       }
       next(createError(401, 'Not authorized'));
     }
   } catch (error) {
+    if (req.path === '/logout') {
+      req.user = { _id: null };
+      return next();
+    }
     next(error);
   }
 };
